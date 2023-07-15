3054278ce153003ca771687337aeae7fa1bb5b5f
Ravindu Hasanka <rhatu2000@gmail.com>
2023-07-16 01:17:33 +0530

    Question 01 senario 02

--- activity.js
+++ activity.js
@@ -1,4 +1,6 @@
 // ---------------------Question 01---------------------
+// Sinario 01
+console.log("-----Question 01 - Sinario 01\n")
 class student{
     
     constructor(Index,Name,DOB,Address,Contact,Email){
@@ -40,3 +42,76 @@ console.log(student01.getStudent())
 student01.setGroup()
 console.log("The group No is",student01.showGroupNo())
 
+// sinario 02
+console.log("\n\n\n-----Question 01 - Sinario 02\n")
+class marks{
+    constructor(Index, MCQ, Essay){
+        this.indexNo = Index
+        this.mcqMarks = MCQ
+        this.essayMarks = Essay
+        // calculateTotal()
+        // calculateGrade()
+        // calculateResult()
+    }
+
+    calculateTotal(){
+        this.totalMarks = this.mcqMarks + this.essayMarks
+    }
+
+    calculateGrade(){
+        if(this.totalMarks>=75){
+            this.grade = 'A'
+        }
+        else if(this.totalMarks>=65){
+            this.grade = 'B'
+        }
+        else if(this.totalMarks>=55){
+            this.grade = 'C'
+        }
+        else if(this.totalMarks>=35){
+            this.grade='S'
+        }
+        else{
+            this.grade='W'
+        }
+    }
+    calculateResult(){
+        if(this.totalMarks>=50){
+            this.result = "Pass"
+        }
+        else{
+            this.result = "Fail"
+        }
+    }
+    setMarks(Index, MCQ, Essay){
+        this.indexNo = Index
+        this.mcqMarks = MCQ
+        this.essayMarks = Essay
+        // calculateTotal()
+        // calculateGrade()
+        // calculateResult()
+    }
+    getmarks(){
+        return this
+    }
+    displayTotal(){
+        console.log("The total is",this.totalMarks)
+    }
+    displayResult(){
+        this.calculateResult()
+        console.log("The result is",this.result)
+    }
+    displayGrade(){
+        this.calculateGrade()
+        console.log("The grade is",this.grade)
+    }
+}
+
+const marks1 = new marks
+marks1.setMarks(1,35,40)
+console.log(marks1.getmarks())
+marks1.calculateTotal()
+console.log(marks1.getmarks())
+marks1.displayTotal()
+marks1.displayResult()
+marks1.displayGrade()
